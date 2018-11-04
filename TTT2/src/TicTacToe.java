602574d86e377a11ed322ba542434ffa6ee8e148
sidney-maass <44192771+sidney-maass@users.noreply.github.com>
2018-11-04 16:06:32 +0100

    Add files via upload

---
+++ TTT2/src/TicTacToe.java
@@ -0,0 +1,12 @@
+
+import java.util.*;
+
+public class TicTacToe {
+	static Scanner sc;
+	public static void main(String[] args) {
+	sc = new Scanner(System.in);
+	
+	int zufallszahl = (int) ((Math.random()*30)+1);
+	System.out.println(zufallszahl);
+}
+}
